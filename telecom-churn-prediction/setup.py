b5be5ec45c35d8e8a0b6da6b58949ba0291a851c
Viji-Veer <163464470+Viji-Veer@users.noreply.github.com>
2025-03-18 00:18:03 +0200

    Update setup.py

---
+++ telecom-churn-prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='telecom_churn',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Telecom customer churn prediction',
+    author='Your Name',
+    license='MIT',
+)
